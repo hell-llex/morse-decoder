a38287d78b94716f4d54e51d35b9fba142eecfc7
hell_llex <demeshenko333@gmail.com>
2022-02-23 01:31:58 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,43 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(ex) {
+    let z = []
+    let mo = ''
+
+    for (let i = 0; i < ex.length; i+=10) {
+        let b = i+10
+        let x = [...ex.slice(i,b)]
+        let n = []
+        if(ex.slice(i,b) != '**********'){
+            for (let v = 0; v < 10; v+=2) {
+                let c = v+1
+                if(+x[v] + +x[c] == 1){
+                    n.push('.')
+                }else if(+x[v] + +x[c] == 2){
+                    n.push('-')
+                }else if(+x[v] + +x[c] == 0){
+                }
+            }
+        } else {
+            n.push('***')
+        }
+        z.push(n.join(''))
+    }
+    console.log(z)
+    z.forEach((elem, i) => {
+        if(elem != '***'){
+            for (let key in MORSE_TABLE) {
+                if(elem == key){
+                    mo += MORSE_TABLE[key]
+                }
+            }
+        } else {
+            mo += ' '
+        }
+
+    })
+    return mo
 }
 
 module.exports = {
